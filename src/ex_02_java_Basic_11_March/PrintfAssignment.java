25cbcf3bfa9e8ae627592ba8427793d8ea9f2678
lonewolfdp <dsprasad.1996@gmail.com>
2025-03-14 23:52:45 +0530

    11-March-2025 Java basic lab assignement

---
+++ src/ex_02_java_Basic_11_March/PrintfAssignment.java
@@ -0,0 +1,20 @@
+package ex_02_java_Basic_11_March;
+
+public class PrintfAssignment {
+    public static void main(String[] args) {
+        int a= 9;
+        int b= 1;
+
+        System.out.printf("9 x 1 = %d %n", a*b);
+        System.out.printf("9 x 2 = %d %n", a*2);
+        System.out.printf("9 x 3 = %d %n", a*3);
+        System.out.printf("9 x 4 = %d %n", a*4);
+        System.out.printf("9 x 5 = %d %n", a*5);
+        System.out.printf("9 x 6 = %d %n", a*6);
+        System.out.printf("9 x 7 = %d %n", a*7);
+        System.out.printf("9 x 8 = %d %n", a*8);
+        System.out.printf("9 x 9 = %d %n", a*9);
+        System.out.printf("9 x 10 = %d %n", a*10);
+
+    }
+}
